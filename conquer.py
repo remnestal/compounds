5a4a12fe2fd019d24a5877f70009d89981a72b19
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-20 01:03:36 +0100

    improve reverse compound heuristic

--- conquer.py
+++ conquer.py
@@ -13,23 +13,25 @@ result = dict()
 for letter in listdir(LEXICON_PATH):
     with open('{}/{}'.format(LEXICON_PATH, letter)) as partition:
         content = partition.readlines()
-        content = [w.strip() for w in content]
+        content = [w.strip() for w in content if len(w) > 1]
         content.sort()
         lexicon[letter] = content
+
+in_lexicon = lambda x: lexicon[x[0]][bisect.bisect(lexicon[x[0]], x)-1] == x
 
 # try separate all words to find reversed compounds
 for letter, partition in lexicon.items():
     for word in partition:
-        # the word may have already been discovered as a reversed compund
+        # the word may have already been discovered as a reversed compound
         if word not in result:
 
             # analyse every possible compound subset
             for i in range(1, len(word)):
                 head, tail = word[:i], word[i:]
-                guess = '{}{}'.format(tail, head)
+                reverse = '{}{}'.format(tail, head)
 
-                # there is a match if the reversed pair is found in the dictionary
-                korpus = lexicon[tail[0]]
-                idx = bisect.bisect(korpus, guess)
-                if korpus[idx-1] == guess:
-                    print((word, guess))
+                if not in_lexicon(head) or not in_lexicon(tail):
+                    continue
+                if in_lexicon(reverse):
+                    result[reverse] = (word, reverse)
+                    print(result[reverse])
